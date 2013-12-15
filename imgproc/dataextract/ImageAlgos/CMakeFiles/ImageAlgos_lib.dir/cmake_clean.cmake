6830b13e08ec3dca3bbb032da7bff9702494ed8f
Timothy Yong <tyong_23@hotmail.com>
2013-12-15 11:18:37 -0500

    corrected dataextract

---
+++ imgproc/dataextract/ImageAlgos/CMakeFiles/ImageAlgos_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ImageAlgos_lib.dir/ImageAlgos.cpp.o"
+  "../libs/libImageAlgos_lib.pdb"
+  "../libs/libImageAlgos_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ImageAlgos_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
