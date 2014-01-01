2539732e1d2674e912b44a6d57c2db22f57122bc
Timothy Yong <tyong_23@hotmail.com>
2014-01-01 08:05:11 -0500

    substituted new stream parser into the queuemanager

--- production/queueManager.py
+++ production/queueManager.py
@@ -25,144 +25,132 @@ time_interval = 90
 inGame = True
 
 def exit(msg):
-	sys.exit()
+    sys.exit()
 
 def onPad(obj, pad, target):
-	sinkpad = target.get_compatible_pad(pad, pad.get_caps())
-	if sinkpad:
-		pad.link(sinkpad)
-		return True
+    sinkpad = target.get_compatible_pad(pad, pad.get_caps())
+    if sinkpad:
+        pad.link(sinkpad)
+        return True
 
 class LivestreamerPlayer(object):
-	def __init__(self):
-		self.fd = None
-		self.mainloop = gobject.MainLoop()
+    def __init__(self):
+        self.fd = None
+        self.mainloop = gobject.MainLoop()
 
-		# This creates a playbin pipeline and using the appsrc source
-		# we can feed it our stream data
-		self.pipeline = gst.Pipeline("stream")
-	   
-		self.appsrc = gst.element_factory_make("appsrc", None)
-		self.decodebin = gst.element_factory_make("decodebin", None)
-		self.videorate = gst.element_factory_make("videorate", None)
-		#self.videoratecap = gst.Caps("video/x-raw-yuv, framerate=4/1")
-		#self.videoratefilter = gst.element_factory_make("capsfilter", "filter")
-		self.ffmpegcolorspace = gst.element_factory_make("ffmpegcolorspace", None)
-		self.jpegenc = gst.element_factory_make("jpegenc", None)
-		self.multifilesink = gst.element_factory_make("multifilesink", None)
-	  
-		#self.videoratefilter.set_property("caps", self.videoratecap)
-		self.videorate.set_property("max-rate", 4)
-		self.multifilesink.set_property("location", "img%d.jpg")
+        self.pipeline = gst.element_factory_make("playbin2", None)
+        self.pipeline.set_property("uri", "appsrc://")
+
+        self.parsebin = gst.Pipeline()
+        videorate = gst.element_factory_make("videorate", None)
+        videorate.set_property("max-rate", 4) 
+        jpegenc = gst.element_factory_make("jpegenc", None)
+        multifilesink = gst.element_factory_make("multifilesink", None)
+        multifilesink.set_property("location", "img%d.jpg")
+        self.parsebin.add(videorate)
+        self.parsebin.add(jpegenc)       
+        self.parsebin.add(multifilesink) 
+        videorate.link(jpegenc)      
+        jpegenc.link(multifilesink)
+        
+        pad = videorate.get_pad("sink");
+        ghost_pad = gst.GhostPad("sink", pad);
+        self.parsebin.add_pad(ghost_pad);
+
+        self.pipeline.set_property("video-sink", self.parsebin)
  
-		self.pipeline.add(self.appsrc)  
-		self.pipeline.add(self.decodebin)  
-		self.pipeline.add(self.videorate)
-		self.pipeline.add(self.ffmpegcolorspace)  
-		self.pipeline.add(self.jpegenc)
-		self.pipeline.add(self.multifilesink)
+#        self.pipeline.cconnect("need-data", self.on_source_need_data)
+        self.pipeline.connect("source-setup", self.on_source_setup)
 
-		self.appsrc.link(self.decodebin)
-		self.decodebin.connect("pad-added", onPad, self.videorate)
-		#self.videorate.link(self.videoratefilter)
-		self.videorate.link(self.ffmpegcolorspace)
-		self.ffmpegcolorspace.link(self.jpegenc)
-		self.jpegenc.link(self.multifilesink)
+        # Creates a bus and set callbacks to receive errors
+        self.bus = self.pipeline.get_bus()
+        self.bus.add_signal_watch()
+        self.bus.connect("message::eos", self.on_eos)
+        self.bus.connect("message::error", self.on_error)
 
-		# When the playbin creates the appsrc source it will call
-		# this callback and allow us to configure it
+    def exit(self, msg):
+        self.stop()
+        exit(msg)
 
-		self.appsrc.connect("need-data", self.on_source_need_data)
+    def stop(self):
+        # Stop playback and exit mainloop
+        self.pipeline.set_state(gst.STATE_NULL)
+        self.mainloop.quit()
 
-		# Creates a bus and set callbacks to receive errors
-		self.bus = self.pipeline.get_bus()
-		self.bus.add_signal_watch()
-		self.bus.connect("message::eos", self.on_eos)
-		self.bus.connect("message::error", self.on_error)
+        # Close the stream
+        if self.fd:
+            self.fd.close()
 
-	def exit(self, msg):
-		self.stop()
-		exit(msg)
+    def play(self, stream):
+        # Attempt to open the stream
+        try:
+            self.fd = stream.open()
+        except StreamError as err:
+            self.exit("Failed to open stream: {0}".format(err))
 
-	def stop(self):
-		# Stop playback and exit mainloop
-		self.pipeline.set_state(gst.STATE_NULL)
-		self.mainloop.quit()
+        # Start playback
+        print("set to playing")
+        self.pipeline.set_state(gst.STATE_PLAYING)
 
-		# Close the stream
-		if self.fd:
-			self.fd.close()
+    def on_source_setup(self, element, source):
+        # When this callback is called the appsrc expects
+        # us to feed it more data
+        source.connect("need-data", self.on_source_need_data)
 
-	def play(self, stream):
-		# Attempt to open the stream
-		try:
-			self.fd = stream.open()
-		except StreamError as err:
-			self.exit("Failed to open stream: {0}".format(err))
+    def on_source_need_data(self, source, length):
+        # Attempt to read data from the stream
+        try:
+            data = self.fd.read(length)
+        except IOError as err:
+            self.exit("Failed to read data from stream: {0}".format(err))
 
-		# Start playback
-		print("set to playing")
-		self.pipeline.set_state(gst.STATE_PLAYING)
+        # If data is empty it's the end of stream
+        if not data:
+            source.emit("end-of-stream")
+            return
 
-	def on_source_setup(self, element, source):
-		# When this callback is called the appsrc expects
-		# us to feed it more data
-		source.connect("need-data", self.on_source_need_data)
+        # Convert the Python bytes into a GStreamer Buffer
+        # and then push it to the appsrc
+        buf = gst.Buffer(data)
+        source.emit("push-buffer", buf)
 
-	def on_source_need_data(self, source, length):
-		# Attempt to read data from the stream
-		try:
-			data = self.fd.read(length)
-		except IOError as err:
-			self.exit("Failed to read data from stream: {0}".format(err))
+    def on_eos(self, bus, msg):
+        # Stop playback on end of stream
+        self.stop()
 
-		# If data is empty it's the end of stream
-		if not data:
-			source.emit("end-of-stream")
-			return
-
-		# Convert the Python bytes into a GStreamer Buffer
-		# and then push it to the appsrc
-		buf = gst.Buffer(data)
-		source.emit("push-buffer", buf)
-
-	def on_eos(self, bus, msg):
-		# Stop playback on end of stream
-		self.stop()
-
-	def on_error(self, bus, msg):
-		# Print error message and exit on error
-		error = msg.parse_error()[1]
-		self.exit(error)
+    def on_error(self, bus, msg):
+        # Print error message and exit on error
+        error = msg.parse_error()[1]
+        self.exit(error)
 
 def getStream(url, quality):
-	livestreamer = Livestreamer()
-	livestreamer.set_loglevel("info")
-	livestreamer.set_logoutput(sys.stdout)
+    livestreamer = Livestreamer()
+    livestreamer.set_loglevel("info")
+    livestreamer.set_logoutput(sys.stdout)
 
-	# Attempt to find a plugin for this URL
-	try:
-		plugin = livestreamer.resolve_url(url)
-	except NoPluginError:
-		exit("Livestreamer is unable to handle the URL '{0}'".format(url))
+    # Attempt to find a plugin for this URL
+    try:
+        plugin = livestreamer.resolve_url(url)
+    except NoPluginError:
+        exit("Livestreamer is unable to handle the URL '{0}'".format(url))
 
-	# Attempt to fetch streams
-	try:
-		streams = plugin.get_streams()
-	except PluginError as err:
-		exit("Plugin error: {0}".format(err))
+    # Attempt to fetch streams
+    try:
+        streams = plugin.get_streams()
+    except PluginError as err:
+        exit("Plugin error: {0}".format(err))
 
-	if len(streams) == 0:
-		exit("No streams found on URL '{0}'".format(url))
+    if len(streams) == 0:
+        exit("No streams found on URL '{0}'".format(url))
 
-	# Look for specified stream
-	if quality not in streams:
-		exit("Unable to find '{0}' stream on URL '{1}'".format(quality, url))
+    # Look for specified stream
+    if quality not in streams:
+        exit("Unable to find '{0}' stream on URL '{1}'".format(quality, url))
 
-	# We found the stream
-	stream = streams[quality]
+    # We found the stream
+    stream = streams[quality]
 
-	return stream
+    return stream
 
 class QueueNode():
 	def __init__(self, data = None, Next = None, timestamp = 0):
@@ -230,7 +218,7 @@ class ImageBuilder(threading.Thread):
 					if "true" in output:
 						inGame = True
 						# need generation of the names.txt file via something (a call possibly?)
-					"""os.system("rm " + imageFilename)"""
+					os.system("rm " + imageFilename)
 				else:
 					# once image has been found, add to queue, as well as remove extra stuff
 					self.queue.lock.acquire()
@@ -272,7 +260,7 @@ class ScoreboardDetect(threading.Thread):
 				except Exception, e:
 					print ("\nError has occurred within the second thread's subprocess", imageFilename[0])
 					print (str(e) + "\n")
-				"""os.system("rm " + imageFilename[0]) # clears the folder"""
+				os.system("rm " + imageFilename[0]) # clears the folder"""
 
 class DataExtract(threading.Thread):
 	def __init__(self, threadID, extractQueue, dataObject):
@@ -306,7 +294,7 @@ class DataExtract(threading.Thread):
 				except Exception, e:
 					print ("\nError has occurred within the third thread's subprocess", imageFilename[0])
 					print (str(e) + "\n")
-				#os.system("rm " + imageFilename)
+				os.system("rm " + imageFilename)
 
 def main():
 	args = [arg for arg in sys.argv]
