dd4c01aaad27846f4dd773d61f1e1f5854df5045
Eric Song <regonics@gmail.com>
2014-01-05 01:16:52 -0500

    json parsing of upload data working

--- imgproc/upload/upload.py
+++ imgproc/upload/upload.py
@@ -5,13 +5,27 @@ client = MongoClient('localhost', 27017);
 db = client.loltabber
 scoreboards = db.scoreboards
 
+metadata = {};
+
+#listen for upload writes
 while True:
-    f = open('/dev/shm/loltab_pipe')
+    f = open('upload_pipe')
     jsonstring = ""
     for line in f.readlines():
-        if("!!!!!" in line):
-            decoded = json.loads(jsonstring)
-            print(scoreboards.insert(decoded))
+        print(line)
+        if("!!!UPDATE!!!"):
+            mdfile = open('metadata.txt')
+            mdstring = ""
+            for line in mdfile.readlines():
+                mdstring += line
+            metadata = json.loads(mdstring)
+            print("Metadata updated")
+            print(metadata) 
+        if("!!!END!!!" in line):
+            decoded = json.loads(jsonstring.strip())
+            decoded['streamer'] = 'meteos'
+            print(decoded)
+        elif("!!!START!!!" in line):
             jsonstring = ""
         else:
             jsonstring += line
--- production/upload.py
+++ production/upload.py
@@ -5,15 +5,26 @@ client = MongoClient('localhost', 27017);
 db = client.loltabber
 scoreboards = db.scoreboards
 
+metadata = {};
+
+#listen for upload writes
 while True:
     f = open('upload_pipe')
     jsonstring = ""
     for line in f.readlines():
+        print(line)
+        if("!!!UPDATE!!!"):
+            mdfile = open('metadata.txt')
+            mdstring = ""
+            for line in mdfile.readlines():
+                mdstring += line
+            metadata = json.loads(mdstring)
+            print("Metadata updated")
+            print(metadata) 
         if("!!!END!!!" in line):
             decoded = json.loads(jsonstring.strip())
-            decoded['streamer'] = 'TheOddOne'
+            decoded['streamer'] = 'meteos'
             print(decoded)
-            #print(scoreboards.insert(decoded))
         elif("!!!START!!!" in line):
             jsonstring = ""
         else:
